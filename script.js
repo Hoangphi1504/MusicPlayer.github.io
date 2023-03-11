3edf8434c7b6d030d04ae89d3100a5c09366209e
Hoangphi1504 <127370214+Hoangphi1504@users.noreply.github.com>
2023-03-12 00:00:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/*
-design by Voicu Apostol.
-design: https://dribbble.com/shots/3533847-Mini-Music-Player
-I can't find any open music api or mp3 api so i have to download all musics as mp3 file.
-You can fork on github: https://github.com/muhammederdem/mini-player
-*/
-
 new Vue({
   el: "#app",
   data() {
@@ -19,75 +12,52 @@ new Vue({
         {
           name: "yêu anh đi mẹ anh bán bánh mì",
           artist: "Phúc Du",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
-          source: "./1.mp3",
+          cover: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/1.jpg",
+          source: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/1.mp3",
           url: "https://youtu.be/-7sISWuTdj0",
           favorited: false
         },
         {
-          name: "Everybody Knows",
-          artist: "Leonard Cohen",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
-          url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
+          name: "Your Smile",
+          artist: "Obito ft. hnhngan",
+          cover: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/2.jpg",
+          source: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/2.mp3",
+          url: "https://www.youtube.com/watch?v=ZqDBgYPpUTg",
           favorited: true
         },
         {
-          name: "Extreme Ways",
-          artist: "Moby",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
-          url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
+          name: "Dear",
+          artist: "Gonzo",
+          cover: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/3.jpg",
+          source: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/3.mp3",
+          url: "https://www.youtube.com/watch?v=kSRTQv1qEno",
           favorited: false
         },
         {
-          name: "Butterflies",
-          artist: "Sia",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
-          url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
+          name: "May be you don't khow me",
+          artist: "Ofenbach ft. Brodie Barclay",
+          cover: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/4.jpg",
+          source: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/4.mp3",
+          url: "https://www.youtube.com/watch?v=sJ9bco27uxsY",
           favorited: false
         },
         {
-          name: "The Final Victory",
-          artist: "Haggard",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
-          url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
+          name: "Thu Cuối",
+          artist: "MR T X YANBI X HẰNG BINGBOONG",
+          cover: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/5.jpg",
+          source: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/5.mp3",
+          url: "https://www.youtube.com/watch?v=A2jn_O7JKjI",
           favorited: true
         },
         {
-          name: "Genius ft. Sia, Diplo, Labrinth",
-          artist: "LSD",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
-          url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
+          name: "Chúng Ta Của Hiện Tại",
+          artist: "Sơn Tùng M-TP",
+          cover: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/6.jpg",
+          source: "https://raw.githubusercontent.com/Hoangphi1504/MusicPlayer.github.io/master/6.mp3",
+          url: "https://www.youtube.com/watch?v=LjSoHSTl7K4",
           favorited: false
         },
-        {
-          name: "The Comeback Kid",
-          artist: "Lindi Ortega",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
-          url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
-          favorited: true
-        },
-        {
-          name: "Overdose",
-          artist: "Grandson",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
-          url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
-          favorited: false
-        },
-        {
-          name: "Rag'n'Bone Man",
-          artist: "Human",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
-          url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
-          favorited: false
-        }
+        
       ],
       currentTrack: null,
       currentTrackIndex: 0,
